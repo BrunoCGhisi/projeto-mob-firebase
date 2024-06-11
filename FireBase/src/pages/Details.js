493bcc6d677e330bebc6057e1e9fd323265c4e70
BrunoCGhisi <brunocostaghisi@gmail.com>
2024-06-11 13:41:37 -0300

    first step

--- FireBase/src/pages/Details.js
+++
@@ -1,7 +0,0 @@
-import { Text } from "react-native";
-
-export default function Details(){
-    return(
-        <Text>Maria melhor amiga do mundo</Text>
-    )
-}
--- FireBase/src/pages/NewTask.js
+++
@@ -1,7 +0,0 @@
-import { Text } from "react-native";
-
-export default function NewTask(){
-    return(
-        <Text>Bruno loirona do tchan</Text>
-    )
-}
